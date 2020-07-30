1c71231f0989e1fd6b4b2a9c33077ebfcfc60497
Lucas Ortigara Reis <lucas@jornadatecnologia.com>
2020-07-30 01:08:08 -0300

    Removido o acompamento das classes com injeção de dependência (DI)

--- Startup.cs
+++ Startup.cs
@@ -16,18 +16,29 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+          //Transient: objects are always different; a new instance is provided to every controller and every service. 
+          //services.AddTransient<ICatalogo, Catalogo>();
+          //services.AddTransient<IRelatorio, Relatorio>();
+
+          //Scoped: objects are the same within a request, but different across different requests.
+          //services.Scoped<ICatalogo, Catalogo>();
+          //services.Scoped<IRelatorio, Relatorio>();
+
+          //Singleton: objects are the same for every object and every request.
+          var catalogo = new Catalogo();
+          services.AddSingleton<ICatalogo>(catalogo);
+          services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            Catalogo catalogo = new Catalogo();
-            Relatorio relatorio = new Relatorio(catalogo);
+            IRelatorio relatorio = serviceProvider.GetService<IRelatorio>();
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
